519d59b33283db37a773e022160c5938895e2efc
agunggumelar <agunggumelar02@gmail.com>
2021-08-17 17:21:30 +0700

    init

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9167518062e0a1af0535486f4110b364'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca5ee4ff-270e-4ff1-a756-a2ec5ae0deb4'
+};
